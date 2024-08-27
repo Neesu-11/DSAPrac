558b8318f9a5c977598f0b139677538c726d2965
Neesu Prasad <75556042+Neesu-11@users.noreply.github.com>
2024-08-27 06:32:53 +0000

    new commit

---
+++ Learncpp/OperatorOverload.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct Vector3{
+    int x;
+    int y;
+    int z;
+
+   Vector3 operator+ (Vector3 Other){
+    return Vector3 {x += Other.x,
+    y += Other.y,
+    z += Other.z};
+   }
+   Vector3 operator= (Vector3 Other){
+   return Vector3{ x = Other.x,
+    y = Other.y,
+    z = Other.z};
+   }
+};
+
+int main(){
+
+ Vector3 vec1 {1,2,3};
+ Vector3 vec2 {10,20,30};
+
+ Vector3 res = vec1 + vec2;
+
+ cout << res.x << res.y << res.z;
+
+
+    return 0;
+}
